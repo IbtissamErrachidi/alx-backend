91f5cd572093fa960fb49af50603b4164e1757f0
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-30 02:00:01 +0100

    basic Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Handles / route
+    """
     return render_template('0-index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
