c25e8803ceee2f68756533186e423e96bd631ee4
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-08-15 19:36:17 +0000

    LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" LIFO Caching """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache caching system """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Remove the last item added (LIFO)
+            last_key = next(reversed(self.cache_data))
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
